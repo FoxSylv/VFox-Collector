6d86c890ca496f63ed4fe8546fa22dde3efe691d
FoxSylv <atheriault@bowdoin.edu>
2023-06-18 14:15:30 -0400

    Add shrine upgrades' uses.

--- commands/game/fox.js
+++ commands/game/fox.js
@@ -3,16 +3,25 @@ const { User, getProfile } = require('../../utilities/db.js');
 
 function getChance(user) {
     const foxes = user.foxes ?? 0;
-    return 0.97 ** foxes;
+    const blessings = user.upgrades?.blessingCount ?? 0;
+    return (0.97 ** foxes) * (1 + 0.2 * blessings);
 }
 
 function findFoxes(user) {
-    return 1;
+    const helpers = user.upgrades?.helperCount ?? 0;
+    let foxes = 1;
+    for (let i = 0; i < helpers; ++i) {
+        if (Math.random() < 0.5) {
+            ++foxes;
+        }
+    }
+    return foxes;
 }
 
 function getCooldown(user) {
     const foxes = user.foxes ?? 0;
-    return 5000 + foxes;
+    const watchers = user.upgrades?.watcherCount ?? 0;
+    return 5000 + Math.min((100 * (foxes - 40)) * (0.8 ** watchers), 0);
 }
 
 module.exports = {
