ebe3c7ea1dd98fb4af8a4042a8f11cb50893df7d
FoxSylv <atheriault@bowdoin.edu>
2023-06-20 00:15:02 -0400

    Add the dev-only `setfoxes` command

---
+++ commands/game/setfoxes.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { User, getProfile } = require('../../utilities/db.js');
+
+module.exports = {
+    isDev: true,
+	data: new SlashCommandBuilder()
+		.setName("setfoxes")
+		.setDescription("Magically apparate foxes")
+        .addIntegerOption(option =>
+            option.setName("foxes")
+                  .setDescription("Number of foxes to apparate")
+                  .setRequired(true)
+                  .setMinValue(0)
+        ),
+	async execute(interaction) {
+        const user = await getProfile(interaction.user.id);
+        const newFoxes = interaction.options.getInteger("foxes");
+        user.foxes = newFoxes;
+        await interaction.reply(`You now have ${newFoxes} foxes!`);
+        await user.save();
+	}
+};
+
