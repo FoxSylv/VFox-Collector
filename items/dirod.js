67f3a2fdbffdb61b06ef40042dcfd2ac2bc581cb
FoxSylv <atheriault@bowdoin.edu>
2023-08-13 23:27:42 -0400

    Add the divining rod
    This is the last normal item to implement!!!!!!!!
    (unless I come up with something else xdd)

---
+++ items/dirod.js
@@ -0,0 +1,33 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+    emoji: ":magic_wand:",
+    name: "Divining Rod",
+    value: "dirod",
+    description: "Determine the exact rarity of an item",
+    rarity: 6,
+    async onUse(user, interaction, items, slot) {
+        const userItems = user.items.map((item, index) => {
+            if (!item || (index === slot)) {
+                return undefined;
+            }
+            return new StringSelectMenuOptionBuilder()
+                .setLabel(items[item].name)
+                .setDescription(items[item].description)
+                .setValue(index.toString())
+        }).filter(i => i);
+
+        const select = new ActionRowBuilder()
+            .addComponents(new StringSelectMenuBuilder()
+                .setCustomId("dirod-select")
+                .setPlaceholder("Choose an item")
+                .addOptions(...userItems)
+            );
+        const selection = await interaction.reply({content: "Select an item to divine", components: [select]});
+        const response = await selection.awaitMessageComponent({filter: i => i.user.id === interaction.user.id, time: 60000});
+
+        interaction.deleteReply();
+        const item = items[user.items[response.values[0]]];
+        return `The ${item.emoji} ${item.name} has rarity ${item.rarity}`;
+    }
+}
