954c3187ebf25cf41dcd480119a2d091270ed285
FoxSylv <atheriault@bowdoin.edu>
2023-08-16 18:50:00 -0400

    Fix missing colons in chain emoji

--- data/effectData.js
+++ data/effectData.js
@@ -3,7 +3,7 @@ const effectData = {
     color: {emoji: ":art:", name: "Custom Color", isStackable: false},
     glass: {emoji: ":mag_right:", name: "Boosted Item Quantity", isStackable: true},
     sslag: {emoji: ":mag:", name: "Decreased Item Quantity", isStackable: true},
-    chain: {emoji: "chains", name: "Boosted Fox Quantity; Fox And Item Quality Decreased", isStackable: true},
+    chain: {emoji: ":chains:", name: "Boosted Fox Quantity; Fox And Item Quality Decreased", isStackable: true},
     ball: {emoji: ":mirror_ball:", name: "Boosted Fox Quality; Fox And Item Quantity Decreased", isStackable: true},
     micro: {emoji: ":microscope:", name: "Boosted Item Quality; All Other Stats Greatly Decreased", isStackable: true},
     prec: {emoji: ":chopsticks:", name: "Boosted Item Quality", isStackable: true},
