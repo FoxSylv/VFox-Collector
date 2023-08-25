7bc83b6020caefa8e86ce1e12c59d9a3d054b948
FoxSylv <atheriault@bowdoin.edu>
2023-08-25 11:35:30 -0400

    Improve `/tips` navigability

--- commands/game/tips.js
+++ commands/game/tips.js
@@ -4,39 +4,56 @@ const { getColor } = require('../../utilities/getColor.js');
 const { tipData } = require('../../data/tipData.js');
 
 
-function getTipScreen(user, currentTip) {
-    let tipNum;
-    do {
-        tipNum = Math.floor(Math.random() * tipData.length);
-    }
-    while (currentTip === tipNum);
-
+function getTipScreen(user, tipNum) {
     const embed = new EmbedBuilder()
         .setColor(getColor(user))
         .setTitle(`Tip #${tipNum + 1}`)
         .setDescription(tipData[tipNum])
-    const button = new ActionRowBuilder().addComponents(
+    const buttons = new ActionRowBuilder();
+    if (tipNum > 0) {
+        buttons.addComponents(
+            new ButtonBuilder()
+                .setCustomId(`tip.${tipNum - 1}`)
+                .setLabel("Previous")
+                .setStyle(ButtonStyle.Primary)
+        )
+    }
+    if (tipNum < tipData.length - 1) {
+        buttons.addComponents(
+            new ButtonBuilder()
+                .setCustomId(`tip.${tipNum + 1}`)
+                .setLabel("Next")
+                .setStyle(ButtonStyle.Primary)
+        )
+    }
+    buttons.addComponents(
         new ButtonBuilder()
-            .setCustomId(`tip.${tipNum}`)
-            .setLabel("Another Tip")
+            .setCustomId(`randomtip`)
+            .setLabel("Random Tip")
             .setStyle(ButtonStyle.Success)
     );
 
-    return {embeds: [embed], components: [button]};
+    return {embeds: [embed], components: [buttons]};
 }
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("tips")
 		.setDescription("Get helpful hints about the game and how to play it"),
-    buttonValues: tipData.map((tip, index) => `tip.${index}`),
+    buttonValues: ["randomtip"].concat(tipData.map((tip, index) => `tip.${index}`)),
     async buttonPress(user, customId) {
-        const currentTip = customId.split('.')[1];
-        return await getTipScreen(user, currentTip)
+        let currentTip;
+        if (customId === "randomtip") {
+            currentTip = Math.floor(Math.random() * tipData.length);
+        }
+        else {
+            currentTip = customId.split('.')[1];
+        }
+        return await getTipScreen(user, parseInt(currentTip))
     },
 	async execute(interaction) {
         const user = await getProfile(interaction.user.id);
-        await interaction.reply(getTipScreen(user));
+        await interaction.reply(getTipScreen(user, 0));
     }
 };
 
