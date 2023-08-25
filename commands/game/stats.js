bc9b76149be4c86ff46a774fcd73cc54576352d9
FoxSylv <atheriault@bowdoin.edu>
2023-08-24 20:13:12 -0400

    Add basic stat screen

--- commands/game/stats.js
+++ commands/game/stats.js
@@ -1,39 +1,55 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { getProfile } = require('../../utilities/db.js');
-const { shopData } = require('../../data/shopData.js');
 const { foxData } = require('../../data/foxData.js');
-const { countFoxes } = require('../../utilities/countFoxes.js');
 const { getColor } = require('../../utilities/getColor.js');
 
+function toFoxVisual(foxes) {
+    if (!foxes) {
+        return "**None!**";
+    }
+    return foxData.map(type => {
+        if (!foxes[type.dbvalue]) return undefined;
+        return `**${foxes[type.dbvalue]}**${type.emoji}`;
+    }).filter(f => f).join("/");
+}
 
-function getPenScreen(user) {
-    let description = (countFoxes(user.foxes) === 0 && (user.coins ?? 0) === 0) ? "You have no foxes :(\n" : foxData.reduce((acc, type) => {
-        if ((user.foxes[type.value] ?? 0) !== 0) {
-            return acc.concat(`**${user.foxes[type.value]}** ${type.emoji}\n`);
-        }   
-        return acc;
-    }, "");
-    if ((user.coins ?? 0) !== 0) {
-        description = description.concat(`**${user.coins}** :coin:\n`);
-    }   
+function getStatScreen(user) {
+    const hasDetails = user.equips?.activeEffects?.includes("stats") ?? false;
+    const userStats = user.stats ?? {};
+    let description = "";
+    
+    if (hasDetails) {
+        description = description.concat("Detailed Stats - TODO!\n\n");
+    }
 
-    const pen = shopData.find(c => c.value === "pens").upgrades.find(u => u.value === user.equips?.pens);
+    description = description.concat(`Total Hunts: **${userStats.numSearches ?? "None!"}**
+Foxes Found: ${toFoxVisual(userStats.foxesFound)}
+Shrine Purchases: **${userStats.shrinePurchases ?? "None!"}**
+Foxes Forfeited: ${toFoxVisual(userStats.foxesForfeited)}
+Shop Purchases: **${userStats.shopPurchases ?? "None!"}**
+Foxes Sold: ${toFoxVisual(userStats.foxesSold)}
+Items Found: **${userStats.itemsFound ?? "None!"}**
+Items Used: **${userStats.itemsUsed ?? "None!"}**
+Bait Consumed: **${userStats.baitConsumed ?? "None!"}**
+Bait Conserved: **${userStats.baitConserved ?? "None!"}**`);
+
+
     const embed = new EmbedBuilder()
         .setColor(getColor(user))
-        .setTitle(user.equips?.pens ? `${pen.name} -` : "Your Pen - ")
+        .setTitle("Your Stats -")
         .setDescription(description)
-        .setFooter({text: `You have ${user.coins ?? 0}🪙`});;
+        .setFooter({text: `You have ${hasDetails ? "" : "not yet "}unlocked the detailed stat screen!`});;
 
     return {embeds: [embed]};
 }
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("pen")
-		.setDescription("Look inside your fox pen!"),
+		.setName("stats")
+		.setDescription("Look at your info!"),
 	async execute(interaction) {
         const user = await getProfile(interaction.user.id);
-        await interaction.reply(getPenScreen(user));
+        await interaction.reply(getStatScreen(user));
     }
 };
 
