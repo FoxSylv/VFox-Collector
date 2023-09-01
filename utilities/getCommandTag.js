40027412d4e20a17e7adc3bfd2bf03d9d721e8ef
FoxSylv <atheriault@bowdoin.edu>
2023-09-01 13:40:54 -0400

    Clean up `getCommandTag`

--- utilities/getCommandTag.js
+++ utilities/getCommandTag.js
@@ -1,23 +1,5 @@
-const https = require('node:https');
-const { clientId, token } = require('../config.json'); 
-const options = {
-    host: "https://discord.com",
-    path: `/api/v10/applications/${clientId}/commands`,
-    headers: {"Authorization": `Bot ${token}`}
-};
-
-
+/* This file is meant for the future. It will return </NAME:COMMAND_ID> */
 function getCommandTag(name) {
-    https.get(options, (res) => {
-        let data = "";
-        console.log(res);
-        res.on("data", chunk => data = data.concat(chunk));
-        res.on("end", () => {
-            //TODO This is broken and needs work. I just don't know how
-        });
-    }).on('error', (e) => {
-        console.error(e);
-    });
     return `\`${name}\``;
 }
 
